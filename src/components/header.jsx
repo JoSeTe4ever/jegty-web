6752773c47d3fe491697817b52537c6c52dd1531
Parreno <847914@cognizant.com>
2020-09-29 10:57:48 +0200

    Added the modal dialog

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,6 +16,7 @@ export class Header extends Component {
                   <p>
                     {this.props.data ? this.props.data.paragraph : "Loading"}
                   </p>
+                  <div className="subtitle pb-3">Games for teams</div>
                   <a
                     href="#features"
                     className="btn btn-custom btn-lg page-scroll"
