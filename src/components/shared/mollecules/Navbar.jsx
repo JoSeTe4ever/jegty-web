0c37af0884862b6836b288dad37e2a015ea25977
Parreno <847914@cognizant.com>
2020-10-05 16:14:19 +0200

    Added the proper navigation with the dashboard

--- src/components/shared/mollecules/Navbar.jsx
+++ src/components/shared/mollecules/Navbar.jsx
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux'
 import { ReactComponent as IconSvg } from '../../../assets/icons/icono.svg';
 import { Icon } from "./../atoms/Icon";
 import { logValidUser } from './../../../redux/actions/actions'
+import {Avatar} from "./../atoms/Avatar";
+import { useHistory } from "react-router-dom";
 
 import './Mollecules.scss';
 
 export const Navbar = () => {
     const dispatch = useDispatch();
-
+    const history = useHistory();
     return (
         <nav className="navbar navbar-light py-1">
             <a className="navbar-brand pl-4" href="#">
@@ -17,8 +19,12 @@ export const Navbar = () => {
                     <span className="brand-span-container pl-3">Jegty</span>
                 </div>
             </a>
-            <Icon icon="users" aria-hidden="true" onClickCallback={() => console.log("")}></Icon>
-            <Icon icon="sign-out" aria-hidden="true" onClickCallback={() => dispatch(logValidUser(false))}></Icon>
+
+            <div className="iconsContainer">
+                <Icon icon="users" aria-hidden="true" onClickCallback={() => console.log("")}></Icon>
+                <Icon icon="sign-out" aria-hidden="true" onClickCallback={() => dispatch(logValidUser(false))}></Icon>
+            </div>
+            <Avatar onClickCallback={() => history.push('profile')}></Avatar>
         </nav>
     )
 }
--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Switch } from "react-router-dom";
-import  GuardedRoute  from '../shared/GuardedRoute';
+import GuardedRoute from '../shared/GuardedRoute';
 import { Home } from '../views/dashboard/Home';
 import { NotFound } from './NotFound';
+import { Navbar } from "../../components/shared/mollecules/Navbar"
+import { Profile } from "../../components/views/dashboard/Profile"
 
 export const Dashboard = () => {
     const user = null;
     const LoggedRoute = GuardedRoute(true); // this shuold be the user logged in
 
+    // remove backdrop from modal if exists.
+    useEffect(() => {
+        const modalBackgroundArray = document.getElementsByClassName("modal-backdrop fade show")
+        if (modalBackgroundArray && modalBackgroundArray.length > 0) {
+            modalBackgroundArray[0].remove();
+        }
+    }, [])
+
+
     return (
-        <Switch>
-            <LoggedRoute exact path="/" component={Home} />
-            <LoggedRoute path="*">
-                <NotFound></NotFound>
-            </LoggedRoute>
-        </Switch>
+        <React.Fragment>
+            <Navbar></Navbar>
+            <Switch>
+                <LoggedRoute exact path="/" component={Home} />
+                <LoggedRoute exact path="/profile" component={Profile} />
+                <LoggedRoute path="*">
+                    <NotFound></NotFound>
+                </LoggedRoute>
+            </Switch>
+        </React.Fragment>
     )
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
